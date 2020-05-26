4edf4684eaff37d2bb380cc6e209529b829e2778
itsubaki <itsubaki@users.noreply.github.com>
2020-05-26 11:26:15 +0900

    Fix repository test

--- pkg/infrastructure/database/account_cost_repository_test.go
+++ pkg/infrastructure/database/account_cost_repository_test.go
@@ -5,6 +5,8 @@ import (
 	"encoding/hex"
 	"testing"
 
+	"github.com/itsubaki/hermes-lambda/pkg/infrastructure/environ"
+
 	"github.com/itsubaki/hermes-lambda/pkg/infrastructure/handler"
 
 	"github.com/itsubaki/hermes-lambda/pkg/domain"
@@ -12,7 +14,7 @@ import (
 )
 
 func TestAccountCostRepository(t *testing.T) {
-	e := main.Env()
+	e := environ.New()
 	h, _ := handler.New(e.Driver, e.DataSource, e.Database)
 	defer h.Close()
 	r := database.NewAccountCostRepository(h)
--- pkg/infrastructure/database/utilization_repository_test.go
+++ pkg/infrastructure/database/utilization_repository_test.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/itsubaki/hermes-lambda/pkg/infrastructure/environ"
 	"github.com/itsubaki/hermes-lambda/pkg/infrastructure/handler"
 
 	"github.com/itsubaki/hermes-lambda/pkg/interface/database"
 )
 
 func TestUtilizationRepository(t *testing.T) {
-	e := main.Env()
+	e := environ.New()
 	h, _ := handler.New(e.Driver, e.DataSource, e.Database)
 	defer h.Close()
 	r := database.NewUtilizationRepository(h)
